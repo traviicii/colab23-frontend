7f508f2acb6e16f402bd863ae1ce0ecacc48486d
yosef-w <ywolday24@gmail.com>
2023-10-15 13:23:23 -0500

    add background shadow to task modals

--- src/Components/Dashboard/Task.jsx
+++ src/Components/Dashboard/Task.jsx
@@ -31,9 +31,11 @@ export default function Task() {
           + Add a New Task
         </button>
         {isTaskModalOpen && (
-          <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div className="fixed top-0 left-0 right-0 bottom-0 z-50 w-full h-screen p-4 overflow-x-hidden overflow-y-auto bg-black bg-opacity-70">
             <div className="relative w-full max-w-2xl max-h-full mx-auto">
+              {/* Your modal content here */}
               <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                {/* Modal content */}
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                     Add Task Information Below
@@ -82,7 +84,7 @@ export default function Task() {
                   </button>
                   <button
                     type="button"
-                    className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                    className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark-text-gray-300 dark-border-gray-500 dark-hover-text-white dark-hover-bg-gray-600 dark-focus-ring-gray-600"
                     onClick={closeTaskModal}
                   >
                     Cancel
@@ -102,16 +104,17 @@ export default function Task() {
           + Propose a New Meeting
         </button>
         {isMeetingModalOpen && (
-          <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div className="fixed top-0 left-0 right-0 bottom-0 z-50 w-full h-screen p-4 overflow-x-hidden overflow-y-auto bg-black bg-opacity-70">
             <div className="relative w-full max-w-2xl max-h-full mx-auto">
               <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                {/* Modal content */}
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                     Add New Meeting Information
                   </h3>
                   <button
                     type="button"
-                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover-text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover-bg-gray-600 dark:hover-text-white"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover-text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark-hover-bg-gray-600 dark-hover-text-white"
                     onClick={closeMeetingModal}
                   >
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
--- src/Components/Dashboard/Team.jsx
+++ src/Components/Dashboard/Team.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Team() {
   return (
     <div>
-      <p>This is the Team tab</p>
+      
     </div>
   )
 }

96f4c375a31bf1c3fd7ae8e1ec62450dcd1c465d
yosef-w <ywolday24@gmail.com>
2023-10-15 15:30:12 -0500

    add team and resource component to dashboard

--- src/Components/Dashboard/Team.jsx
+++ src/Components/Dashboard/Team.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from 'react';
 
 export default function Team() {
   return (
-    <div>
-      
+    <div className="team-container flex">
+      {/* Card 1 */}
+      <div className="w-1/4 bg-white p-4 rounded-lg shadow-lg text-center mx-4">
+        <img src="https://picsum.photos/200" alt="Profile Pic" className="mx-auto mb-4 w-16 h-16 rounded" />
+        <h2 className="text-xl font-semibold">Team Member Name</h2>
+        <h3 className="text-gray-600">Role</h3>
+        <button className="hover:bg-blue-700 border-2 border-black font-bold py-2 px-4 rounded mt-4 w-full">
+          Send a Message
+        </button>
+        <p className="text-left mt-8">Location: City, State</p>
+        <h3 className="text-left font-bold mt-8">To Do</h3>
+        <ul className="text-left">
+          <li>Need to do this task, Timestamp</li>
+          <li>Need to do this task, Timestamp</li>
+        </ul>
+        <h3 className="text-left font-bold mt-8">Recently Completed</h3>
+        <ul className="text-left">
+          <li>Completed this task, Timestamp</li>
+          <li>Completed this task, Timestamp</li>
+        </ul>
+      </div>
+
+        {/* Card 2 */}
+            <div className="w-1/4 bg-white p-4 rounded-lg shadow-lg text-center mx-4">
+        <img src="https://picsum.photos/200" alt="Profile Pic" className="mx-auto mb-4 w-16 h-16 rounded" />
+        <h2 className="text-xl font-semibold">Team Member Name</h2>
+        <h3 className="text-gray-600">Role</h3>
+        <button className="hover:bg-blue-700 border-2 border-black font-bold py-2 px-4 rounded mt-4 w-full">
+          Send a Message
+        </button>
+        <p className="text-left mt-8">Location: City, State</p>
+        <h3 className="text-left font-bold mt-8">To Do</h3>
+        <ul className="text-left">
+            <li>Need to do this task, Timestamp</li>
+            <li>Need to do this task, Timestamp</li>
+        </ul>
+        <h3 className="text-left font-bold mt-8">Recently Completed</h3>
+        <ul className="text-left">
+            <li>Completed this task, Timestamp</li>
+            <li>Completed this task, Timestamp</li>
+        </ul>
+      </div>
+
+        {/* Card 3 */}
+            <div className="w-1/4 bg-white p-4 rounded-lg shadow-lg text-center mx-4">
+        <img src="https://picsum.photos/200" alt="Profile Pic" className="mx-auto mb-4 w-16 h-16 rounded" />
+        <h2 className="text-xl font-semibold">Team Member Name</h2>
+        <h3 className="text-gray-600">Role</h3>
+        <button className="hover:bg-blue-700 border-2 border-black font-bold py-2 px-4 rounded mt-4 w-full">
+          Send a Message
+        </button>
+        <p className="text-left mt-8">Location: City, State</p>
+        <h3 className="text-left font-bold mt-8">To Do</h3>
+        <ul className="text-left">
+            <li>Need to do this task, Timestamp</li>
+            <li>Need to do this task, Timestamp</li>
+        </ul>
+        <h3 className="text-left font-bold mt-8">Recently Completed</h3>
+        <ul className="text-left">
+            <li>Completed this task, Timestamp</li>
+            <li>Completed this task, Timestamp</li>
+        </ul>
+      </div>
+
+        {/* Card 4 */}
+            <div className="w-1/4 bg-white p-4 rounded-lg shadow-lg text-center mx-4">
+        <img src="https://picsum.photos/200" alt="Profile Pic" className="mx-auto mb-4 w-16 h-16 rounded" />
+        <h2 className="text-xl font-semibold">Team Member Name</h2>
+        <h3 className="text-gray-600">Role</h3>
+        <button className="hover:bg-blue-700 border-2 border-black font-bold py-2 px-4 rounded mt-4 w-full">
+          Send a Message
+        </button>
+        <p className="text-left mt-8">Location: City, State</p>
+        <h3 className="text-left font-bold mt-8">To Do</h3>
+        <ul className="text-left">
+            <li>Need to do this task, Timestamp</li>
+            <li>Need to do this task, Timestamp</li>
+        </ul>
+        <h3 className="text-left font-bold mt-8">Recently Completed</h3>
+        <ul className="text-left">
+            <li>Completed this task, Timestamp</li>
+            <li>Completed this task, Timestamp</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }

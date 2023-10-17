c301f6bd3e6f7cfb5cb07fa62b02c9de4750e6d1
yosef-w <ywolday24@gmail.com>
2023-10-17 11:06:52 -0500

    add new colors

--- src/Views/PersonalDetails/PersonalDetails.jsx
+++ src/Views/PersonalDetails/PersonalDetails.jsx
@@ -92,7 +92,7 @@ export default function PersonalDetails() {
   return (
     <div className="personal-details-container">
       <div className="flex items-center justify-center">
-        <div className="shadow-2xl rounded-xl w-4/5 md:w-4/5 lg:w-3/5 xl:w-2/5 px-4 md:px-6 py-8 md:py-10">
+        <div className="shadow-2xl rounded-xl w-4/5 md:w-4/5 lg:w-3/5 xl:w-2/5 px-4 md:px-6 py-8 md:py-10 bg-white">
           <div className="mb-4">
             <div className="w-full bg-gray-200 h-2 rounded-full mr-20">
               <div className="bg-blue-500 h-2 rounded-full" style={{ width: '16.67%' }}></div>
--- src/Views/ProfessionalBackground/ProfessionalBackground.jsx
+++ src/Views/ProfessionalBackground/ProfessionalBackground.jsx
@@ -88,7 +88,7 @@ export default function ProfessionalBackground() {
     <div className="professional-background-container">
       <div className="flex items-center justify-center">
         <div
-          className="shadow-2xl rounded-xl w-4/5 md:w-4/5 lg:w-3/5 xl:w-2/5 px-4 md:px-6 py-8 md:py-10">
+          className="shadow-2xl rounded-xl w-4/5 md:w-4/5 lg:w-3/5 xl:w-2/5 px-4 md:px-6 py-8 md:py-10 bg-white">
           <div className="mb-4 flex items-center">
             <button className="text-blue-500 hover:underline text-lg mr-4" onClick={() => navigate("/personal-details")}>Back</button>
             {/* progress bar */}
